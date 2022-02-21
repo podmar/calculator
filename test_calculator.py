72ad81a598821029acb88fb8511958bd16028396
podmar <marta.podziewska@gmail.com>
2022-02-21 21:43:10 +0100

    added draft test file, not funtional: calculator structure needs rewriting

---
+++ test_calculator.py
@@ -0,0 +1,15 @@
+import unittest
+
+#calculator needs rewriting to be testable: funtions needed for each action plus for returning the right value. 
+from calculator import xxxx
+
+class TestCalculator(unittest.TestCase): 
+    def test_addition(self): 
+        """Testing addition
+        """
+
+        self.assertEqual(first, second)
+        self.assertEqual(first, second)
+
+if __name__ == "__main__": 
+    unittest.main()
