ea4e9999d19f92ba3a9d57089617f75791142236
podmar <marta.podziewska@gmail.com>
2022-02-22 17:57:57 +0100

    added testing for operation (+,-,*,/), convert and calculate functions

--- test_calculator.py
+++ test_calculator.py
@@ -1,15 +1,56 @@
 import unittest
+from calculator import convert, add, subtract, multiply, divide, calculate, calculator_main
 
-#calculator needs rewriting to be testable: funtions needed for each action plus for returning the right value. 
-from calculator import xxxx
+# struggling to write assertions for funtions taking no arguments or returning multiple arguments. 
 
 class TestCalculator(unittest.TestCase): 
-    def test_addition(self): 
-        """Testing addition
+    def test_add(self): 
+        """Testing addition funtion
+        """
+        self.assertEqual(add(3, 9), 12)
+        self.assertEqual(add(-3, 9), 6)
+        self.assertEqual(add(0, 9.5), 9.5)
+
+    def test_subtract(self): 
+        """Testing subtraction funtion
+        """
+        self.assertEqual(subtract(6, 19), -13)
+
+    def test_multiply(self):
+        """Testing multiplication funtion
+        """
+        self.assertEqual(multiply(3.12, 2), 6.24)
+
+    def test_divide(self):
+        """Testing division funtion
+        """
+        self.assertEqual(divide(6.80, 2), 3.4)
+        self.assertEqual(divide(1, 5), 0.2)
+
+    def test_calculate(self): 
+        """Testing the funtion that decides which operation to conduct
         """
 
-        self.assertEqual(first, second)
-        self.assertEqual(first, second)
+        self.assertEqual(calculate(5, -120, "+"), -115)
+        self.assertEqual(calculate(-10, 203, "-"), -213)
+        self.assertEqual(calculate(0.1, 5, "*"), 0.5)
+        self.assertEqual(calculate(10, 20, "/"), 0.5)
+        self.assertEqual(calculate(10, 0, "/"), None)
+    
+    def test_convert(self): 
+        """Testing the function converting the user input
+        """
+        self.assertIsInstance(convert("52+2"), tuple)
+        self.assertEqual(convert("52+2"), (52, 2, "+"))
+        self.assertEqual(convert("5736-12"), (5736, -12, "*"))
+        self.assertEqual(convert("0/2"), (0, 2, "/"))
+        self.assertEqual(convert("-5736/3"), (-5736, 3, "/"))
+        self.assertEqual(convert("2/0"), (2, 0, "/"))
+
+    # def test_main(self): 
+    #     """Testing main calculator funtion
+    #     """
+    #     self.assertEqual(calculator_main(), second)
 
 if __name__ == "__main__": 
     unittest.main()
